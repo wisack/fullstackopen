a87757e69554852296c9e5c5c4763a43fc8547a0
wisack <38721487+wisack@users.noreply.github.com>
2022-12-30 17:20:45 +0200

    Update App.js

--- 1/unicafe/src/App.js
+++ 1/unicafe/src/App.js
@@ -37,7 +37,6 @@ const Button = ({ handleClick, text }) => (
 
 
 const App = () => {
-  // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
